f27cd2d81eef5d135d348d3d5209a45cadcda7ba
mycomind4-arch <mycomind4@gmail.com>
2026-08-13 03:29:33 -0700

    feat: add public core exports

---
+++ src/core/index.ts
@@ -0,0 +1,6 @@
+export * from './types.js';
+export * from './registry.js';
+export * from './orchestrator.js';
+export * from './normalizer.js';
+export * from './search-plan.js';
+export * from './evidence.js';
